2151eeecc7b1e9df1824b57db1e65df611d3c1f5
Casper Schreiter <casper.schreiter@gmail.com>
2016-05-03 17:52:19 +0200

    Some javadoc added in ServerMonitor. Very poorly written javadoc I might add

--- Projekt/src/main/java/server/ServerMonitor.java
+++ Projekt/src/main/java/server/ServerMonitor.java
@@ -16,10 +16,17 @@ public class ServerMonitor {
 		messages = new HashMap<Socket, MessageHandler>();
 	}
 
+	/**
+	 * Adds socket connection to the server monitor. All messages will prpagate to this added socket.
+	 * @param s
+	 */
 	public synchronized void addConnection(Socket s) {
 		connections.add(s);
 	}
-
+	
+	/**
+	 * No more than MAX_CONNECTIONS allowed.
+	 */
 	public synchronized boolean newConnectionPossible() {
 		return connections.size() < MAX_CONNECTIONS;
 	}
@@ -27,16 +34,22 @@ public class ServerMonitor {
 	public synchronized void removeConnection(Socket s) {
 		connections.remove(s);
 	}
-	
+
+	/**
+	 * Adds message to monitor.
+	 * @param s Socket which the messages is affiliated with 
+	 * @param mh message
+	 */
 	public synchronized void addMessage(Socket s, MessageHandler mh) {
 		messages.put(s, mh);
 		notifyAll();
 	}
 
-	public synchronized boolean hasConnections() {
-		return connections.size() > 0;
-	}
-
+	/**
+	 * Returns messages that a specific socket should send out. Messages that comes from s will not be included.
+	 * @param s
+	 * @return
+	 */
 	public synchronized ArrayList<MessageHandler> getMessages(Socket s) {
 		ArrayList<MessageHandler> ret = new ArrayList<MessageHandler>();
 		
